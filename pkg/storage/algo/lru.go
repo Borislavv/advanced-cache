4f0ef4506fd4db19f3bbd5305b1c570ba142aee1
Boris <glazunov2142@icloud.com>
2025-06-01 09:50:23 +0300

    Update lru.go

--- pkg/storage/algo/lru.go
+++ pkg/storage/algo/lru.go
@@ -3,6 +3,7 @@ package algo
 import (
 	"container/list"
 	"context"
+	"fmt"
 	"github.com/Borislavv/traefik-http-cache-plugin/pkg/config"
 	"github.com/Borislavv/traefik-http-cache-plugin/pkg/model"
 	sharded "github.com/Borislavv/traefik-http-cache-plugin/pkg/storage/map"
@@ -114,12 +115,41 @@ func (c *LRUAlgo) evict(ctx context.Context, key uint) {
 
 	memItem := int64(c.shardedMap.Mem()) / c.shardedMap.Len()
 
-	itemsForEviction := int(memDiff/memItem) + c.cfg.ParallelEvictionsAvailable
+	itemsForEviction := int(memDiff/memItem) + (c.cfg.ParallelEvictionsAvailable)
 
 	evicted := c.evictBatch(ctx, key, itemsForEviction)
 	_ = evicted
 
-	log.Debug().Msgf("LRU: evicted %d items (mem: %dKB, len: %d)", evicted, c.shardedMap.Mem()/1024, c.shardedMap.Len())
+	log.Debug().Msgf("LRU: evicted %d items (mem: %s, len: %d)", evicted, formatBytes(c.shardedMap.Mem()), c.shardedMap.Len())
+}
+
+func formatBytes(bytes uintptr) string {
+	const (
+		KB = 1024
+		MB = KB * 1024
+		GB = MB * 1024
+		TB = GB * 1024
+	)
+
+	switch {
+	case bytes >= TB:
+		t := bytes / TB
+		rem := bytes % TB
+		return fmt.Sprintf("%dTB %dGB %dMB %dKB %dB", t, rem/GB, (rem%GB)/MB, (rem%MB)/KB, rem%KB)
+	case bytes >= GB:
+		g := bytes / GB
+		rem := bytes % GB
+		return fmt.Sprintf("%dGB %dMB %dKB %dB", g, rem/MB, (rem%MB)/KB, rem%KB)
+	case bytes >= MB:
+		m := bytes / MB
+		rem := bytes % MB
+		return fmt.Sprintf("%dMB %dKB %dB", m, rem/KB, rem%KB)
+	case bytes >= KB:
+		k := bytes / KB
+		return fmt.Sprintf("%dKB %dB", k, bytes%KB)
+	default:
+		return fmt.Sprintf("%dB", bytes)
+	}
 }
 
 func (c *LRUAlgo) evictBatch(ctx context.Context, shardKey uint, num int) int {
