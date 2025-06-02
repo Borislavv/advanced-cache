dbb0e7fe544eff5eadf7b6edd917ccb9b893d97c
Boris <glazunov2142@icloud.com>
2025-06-02 11:19:39 +0300

    Update response.go

--- pkg/config/response.go
+++ pkg/config/response.go
@@ -7,7 +7,7 @@ type Response struct {
 	// random time point for refresh response (must be from 0.1 to 0.9).
 	// Don't use absolute values like 0 and 1 due it will be leading to CPU peaks usage.
 	//  - beta = 0.5 — regular, good for start value
-	//  - beta = 1.0 — agressive refreshing
+	//  - beta = 1.0 — aggressive refreshing
 	//  - beta = 0.0 — disables refreshing
 	RevalidateBeta     float64       `mapstructure:"REVALIDATE_BETA"`
 	RevalidateInterval time.Duration `mapstructure:"REVALIDATE_INTERVAL"`
