bb09f6d866287af35800a09a0ec958083752cb70
Boris <glazunov2142@icloud.com>
2025-06-07 10:03:07 +0300

    Update storage.go

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -3,6 +3,8 @@ package storage
 import (
 	"context"
 	"github.com/Borislavv/traefik-http-cache-plugin/pkg/storage/cache/lru"
+	sharded "github.com/Borislavv/traefik-http-cache-plugin/pkg/storage/map"
+	"github.com/Borislavv/traefik-http-cache-plugin/pkg/time"
 
 	"github.com/Borislavv/traefik-http-cache-plugin/pkg/config"
 	"github.com/Borislavv/traefik-http-cache-plugin/pkg/model"
@@ -18,11 +20,11 @@ type AlgoStorage struct {
 	Storage
 }
 
-func New(ctx context.Context, cfg *config.Config) *AlgoStorage {
+func New(ctx context.Context, cfg *config.Config, wheel *time.Wheel, shardedMap *sharded.Map[*model.Response]) *AlgoStorage {
 	var s Storage
 	switch cache.Algorithm(cfg.EvictionAlgo) {
 	case cache.LRU:
-		s = lru.NewLRU(ctx, cfg)
+		s = lru.NewLRU(ctx, cfg, wheel, shardedMap)
 	default:
 		panic("algorithm " + cfg.EvictionAlgo + " does not implemented yet")
 	}
