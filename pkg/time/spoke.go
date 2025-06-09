8af16042aab32ea73904e71aee747b3d6790b488
Boris <glazunov2142@icloud.com>
2025-06-09 23:47:39 +0300

    Update spoke.go

--- pkg/time/spoke.go
+++ pkg/time/spoke.go
@@ -1,1 +1,23 @@
 package time
+
+import (
+	"github.com/Borislavv/traefik-http-cache-plugin/pkg/storage/list"
+	"time"
+	"unsafe"
+)
+
+type spoke struct {
+	key               uint64
+	shardKey          uint64
+	element           *list.Element[*spoke]
+	interval          time.Duration
+	mustBeRefreshedAt time.Time
+}
+
+func (s *spoke) IsReady() bool {
+	return s.mustBeRefreshedAt.After(time.Now())
+}
+
+func (s *spoke) Memory() uintptr {
+	return unsafe.Sizeof(s)
+}
