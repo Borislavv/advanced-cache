66497dc1ac765f57f22cee5e22f75079293ac0d3
Boris <glazunov2142@icloud.com>
2025-06-12 16:52:36 +0300

    Update config.go

--- internal/cache/config/config.go
+++ internal/cache/config/config.go
@@ -2,10 +2,12 @@ package config
 
 import (
 	cacheConfig "github.com/Borislavv/traefik-http-cache-plugin/pkg/config"
-	"github.com/Borislavv/traefik-http-cache-plugin/pkg/server/config"
+	prometheusconifg "github.com/Borislavv/traefik-http-cache-plugin/pkg/prometheus/metrics/config"
+	fasthttpconfig "github.com/Borislavv/traefik-http-cache-plugin/pkg/server/config"
 )
 
 type Config struct {
-	config.HttpServer  `mapstructure:",squash"`
-	cacheConfig.Config `mapstructure:",squash"`
+	cacheConfig.Cache        `mapstructure:",squash"`
+	prometheusconifg.Metrics `mapstructure:",squash"`
+	fasthttpconfig.Server    `mapstructure:",squash"`
 }
