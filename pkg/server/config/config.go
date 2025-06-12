d9491cfba6bc458b2e06f4346a8778f31a738774
Boris <glazunov2142@icloud.com>
2025-06-12 17:01:19 +0300

    Update config.go

--- pkg/server/config/config.go
+++ pkg/server/config/config.go
@@ -13,13 +13,13 @@ type Configurator interface {
 
 type Server struct {
 	// ServerName is a name of the shared server.
-	ServerName string `envconfig:"SERVER_NAME" mapstructure:"FASTHTTP_SERVER_NAME" default:"DefLang"`
+	ServerName string `envconfig:"FASTHTTP_SERVER_NAME" mapstructure:"FASTHTTP_SERVER_NAME" default:"@BorislavGlazunov"`
 	// ServerPort is a port for shared server (endpoints like a /probe for k8s).
-	ServerPort string `envconfig:"SERVER_PORT" mapstructure:"FASTHTTP_SERVER_PORT" default:":8020"`
+	ServerPort string `envconfig:"FASTHTTP_SERVER_PORT" mapstructure:"FASTHTTP_SERVER_PORT" default:":8020"`
 	// ServerShutDownTimeout is a duration value before the server will be closed forcefully.
-	ServerShutDownTimeout time.Duration `envconfig:"FASTHTTP_SERVER_SHUTDOWN_TIMEOUT" mapstructure:"SERVER_SHUTDOWN_TIMEOUT" default:"5s"`
+	ServerShutDownTimeout time.Duration `envconfig:"FASTHTTP_SERVER_SHUTDOWN_TIMEOUT" mapstructure:"FASTHTTP_SERVER_SHUTDOWN_TIMEOUT" default:"5s"`
 	// ServerRequestTimeout is a timeout value for close request forcefully.
-	ServerRequestTimeout time.Duration `envconfig:"FASTHTTP_SERVER_REQUEST_TIMEOUT" mapstructure:"SERVER_REQUEST_TIMEOUT" default:"1m"`
+	ServerRequestTimeout time.Duration `envconfig:"FASTHTTP_SERVER_REQUEST_TIMEOUT" mapstructure:"FASTHTTP_SERVER_REQUEST_TIMEOUT" default:"1m"`
 }
 
 func (c Server) GetHttpServerName() string {
